c9959998343af2c0317b8a595063bb1b83c1a49c
mstefanwalker <stefan.walker@penn-interactive.com>
2021-09-02 22:30:58 -0400

    Added oyster

--- src/routes/page.svelte
+++ src/routes/page.svelte
@@ -2,8 +2,25 @@
     Hello
 </h1>
 
+<div class='crop'>
+    <img class='oyster' src='oyster.jpg' alt='a hand drawn oyster'>
+</div>
+
 <style>
     h1 {
         text-align: center;
+    }
+    .crop {
+        margin-left: auto;
+        margin-right: auto;
+        width: 550px;
+        height: 400px;
+        overflow: hidden;
+    }
+    .oyster {
+        display: block;
+        margin: -130px 0 0 8px;
+        width: 550px;
+        height: 440px;
     }
 </style>
